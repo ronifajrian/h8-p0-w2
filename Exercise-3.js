bd3e9f9d0aa2c223d49da531a348e888b778eb82
ronifajrian <ronifajrian@gmail.com>
2018-03-26 22:28:01 +0700

    correct the operator symbol

--- Exercise-3.js
+++ Exercise-3.js
@@ -1,19 +1,19 @@
 var nama = "Mikael";
 var peran = "";
 
-if (nama == "") {
+if (nama === "") {
   console.log("Nama harus diisi!");
-} else if (peran == "") {
+} else if (peran === "") {
   console.log("Hallo " + nama + ", Pilih peranmu untuk memulai game!");
-} else if (peran == "Ksatria") {
+} else if (peran === "Ksatria") {
   console.log("Selamat datang di dunia Proxytia, " + nama);
   console.log("Hallo " + peran + " " + nama +
   ", kamu dapat menyerang dengan senjata mu!");
-} else if (peran == "Tabib") {
+} else if (peran === "Tabib") {
   console.log("Selamat datang di dunia Proxytia, " + nama);
   console.log("Hallo " + peran + " " + nama +
   ", kamu akan membantu temanmu yang terluka.");
-} else if (peran == "Penyihir") {
+} else if (peran === "Penyihir") {
   console.log("Selamat datang di dunia Proxytia, " + nama);
   console.log("Hallo " + peran + " " + nama +
   ", ciptakan keajaiban yang membantu kemenanganmu!");
